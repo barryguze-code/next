002dd10d3919bd5d8467824c5697702c309d941e
barryguze-code <barry.guze@gmail.com>
2026-02-20 15:17:18 +0000

    rollback

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-// import path from "path";
-// import { componentTagger } from "lovable-tagger";
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/next/',   // 👈 MUST match repo name
-})
-
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-//export default defineConfig(({ mode }) => ({
-// server: {
-//    host: "::",
-//    port: 8080,
-//    hmr: {
-//      overlay: false,
-//    },
-//  },
-//  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-//  resolve: {
-//    alias: {
-//      "@": path.resolve(__dirname, "./src"),
-//    },
-//  },
-//}));
+export default defineConfig(({ mode }) => ({
+ server: {
+    host: "::",
+    port: 8080,
+    hmr: {
+      overlay: false,
+    },
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
 
